839275a62948b6c81c2689154721fde552eccc04
keshav jha <keshavjhaa2678@gmail.com>
2020-11-26 11:14:21 +0530

    Update BeautifulDays.py

--- DAY-4/BeautifulDays.py
+++ DAY-4/BeautifulDays.py
@@ -1,9 +1,9 @@
 # problem - https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
 # Complete the beautifulDays function below.
+
 def beautifulDays(i, j, k):
     b_days = 0
     for n in range(i,j+1):
-        val = (max(n,int(str(n)[::-1]))-min(n,int(str(n)[::-1])))/k
-        if str(val)[-1]=="0":
+        if (max(n,int(str(n)[::-1]))-min(n,int(str(n)[::-1])))%k==0:
             b_days+=1
     return b_days
