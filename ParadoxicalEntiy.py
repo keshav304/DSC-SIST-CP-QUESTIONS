023f0bf7e72dd6266ffd247c549df969864c9b9d
keshav jha <keshavjhaa2678@gmail.com>
2020-11-29 20:38:01 +0530

    Create ParadoxicalEntiy.py

---
+++ ParadoxicalEntiy.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/contests/dsc-sist-competitive-coding-cookoff/challenges/paradoxical-entity
+
+n=int(input())
+arr=list(map(int, input().split()))
+x=int(input())
+
+element = arr[x-1]
+arr.sort()
+i = arr.index(element)
+print(i+1)
